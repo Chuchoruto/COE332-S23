f8cc261a3c58f70e47076a573fff3fb5a44f63a1
Luca <42877969+Chuchoruto@users.noreply.github.com>
2023-01-19 11:31:46 -0600

    Delete directory to move files

--- homework01/script03.py
+++
@@ -1,9 +0,0 @@
-import names
-
-def lenname (nombre):
-    return len(nombre) - 1
-
-losnames = []
-for i in range(5):
-    losnames.append(names.get_full_name())
-    print(losnames[i] + ' ' + str(lenname(losnames[i])))
